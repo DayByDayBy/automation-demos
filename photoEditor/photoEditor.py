0fd688c49ca3ccef0f221722a20527a35a87e4e3
Diomedes Git <diomedesgit@gmail.com>
2023-03-27 16:58:43 +0100

    fleshed out a basic photo editor script that makes stuff black and white, boost contrast, and enhances edges. also skips .DSStore file, so it will work on macOS

---
+++ photoEditor/photoEditor.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageFilter, ImageEnhance, ImageColor
+import os
+
+path = "./img"                                                  # local folder for unedited images
+pathOut = "/editedImgs"                                         # local folder for edited images
+
+for filename in os.listdir(path):                               # for a given image in the first folder
+    if filename == '.DS_Store':                                 # skips .DS_Store file, which macOS creates when using finder
+        continue
+    img = Image.open(f"{path}/{filename}")                      # open the image
+    edit = img.filter(ImageFilter.EDGE_ENHANCE).convert("L")    # filter - enhance edges, convert to monochrome
+    factor = 1.5                                                # set a factor amount
+    enhancer = ImageEnhance.Contrast(edit)                      # create a contrast enhancer
+    edit = enhancer.enhance(factor)                             # use the enhancer by preset factor 
+    clean_name = os.path.splitext(filename)[0]                  # splts filename and extension, creating tuple
+    edit.save(f'.{pathOut}/{clean_name}_edited.png')            # save the edited image to the local folder, using prev tuple to create name
